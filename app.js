f2cedf1830b815b2acd0818cbcf14dc0aea81809
Aleksandra <a.haniok@gmail.com>
2022-11-01 18:55:47 +0000

    Add work list, giving two separate data

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser')
 const app = express();
 
 let items = ['Buy food', 'Cook food', 'Eat food'];
+let workItems = [];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));  // required in app.post method
@@ -24,16 +25,33 @@ app.get('/', (req, res) => {
 
     let day = today.toLocaleDateString('en-US', options);
      
-    res.render('list', {kindOfDay: day, newListItem: items})
+    res.render('list', {listTitle: day, newListItem: items})
 });
+
 
 app.post('/', (req, res) => {
+
     let item = req.body.newItem
-
-    items.push(item);
-
-    res.redirect('/');
+    if (req.body.list === 'Work') {
+        workItems.push(item)
+        res.redirect('/work');
+    } else {
+        items.push(item);
+        res.redirect('/');
+    }
 });
+
+
+app.get('/work', (req, res) => {
+    res.render('list', {listTitle: 'Work List', newListItem: workItems});
+})
+
+app.post('/work', (req, res) => {
+    let item = req.body.newItem
+    workItems.push(item);
+
+    res.redirect('/work');
+})
 
 app.listen(3000, () => {
   console.log('Server started on port 3000');
