27819b52bc4b135631e980ff8948209710ed6992
Aleksandra <a.haniok@gmail.com>
2022-11-04 21:03:42 +0000

    Create custom lists using express route parameters

--- app.js
+++ app.js
@@ -40,6 +40,16 @@ const item3 = new Item({
 const defaultItems = [item1, item2, item3];
 
 
+// Create a new schema for todo lists
+const listSchema = {
+    name: String,
+    items : [itemsSchema]
+};
+
+// Create mongoose model
+const List = mongoose.model("List", listSchema);
+
+
 // Request when the page is loaded
 app.get('/', (req, res) => {
 
@@ -58,8 +68,34 @@ app.get('/', (req, res) => {
             res.render('list', { listTitle: 'Today', newListItems: foundItems });
         }
     })
-
 });
+
+// CREATE CUSTOM TO-DO LIST USING ROUTE PARAMETER
+app.get("/:customListName", function(req, res) {
+    const customListName = req.params.customListName;
+   
+    List.findOne({name: customListName}, function(err, foundList) {
+      if(!err){
+        if(!foundList){
+          // Create New List
+          const list = new List({
+            name: customListName,
+            items: defaultItems
+          });
+   
+          list.save(() => res.redirect('/' + customListName));
+
+        } else {
+          // Show Existing List
+          res.render("list", {
+              listTitle: foundList.name,
+              newListItems: foundList.items
+            });
+        }
+      }
+    });
+  });
+
 
 app.post('/', (req, res) => {
     const itemName = req.body.newItem;
@@ -85,19 +121,6 @@ app.post('/delete', (req, res) => {
     })
 })
 
-app.get('/work', (req, res) => {
-    res.render('list', {
-        listTitle: 'Work List',
-        newListItem: workItems
-    });
-})
-
-app.post('/work', (req, res) => {
-    let item = req.body.newItem
-    workItems.push(item);
-
-    res.redirect('/work');
-})
 
 app.listen(3000, () => {
     console.log('Server started on port 3000');
